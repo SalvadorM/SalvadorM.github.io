d3cd35771dfeb8b17ff00fe94174674ae27c3f9e
SalvadorM <sronikle@gmail.com>
2019-06-24 19:47:50 -0400

    mobile screen layout

---
+++ precache-manifest.4ca3eb077a7ed4bbf25e9b63cde04f75.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6e5771c1911bf64b7064963a15c458",
+    "url": "SalvadorM.github.io/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "SalvadorM.github.io/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "945bdf090ff8db58c92e",
+    "url": "SalvadorM.github.io/static/js/main.fd4db030.chunk.js"
+  },
+  {
+    "revision": "ce8d4503ce5f6242e434",
+    "url": "SalvadorM.github.io/static/js/runtime~main.055b4fd3.js"
+  }
+]);
